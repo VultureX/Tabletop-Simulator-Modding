e00e8bcc647699bc20e98666b251e92a947f1e5d
peter <peter@engine-software.com>
2021-10-17 21:55:15 +0200

    Tests with tablets / settings menu from 2 years ago...

---
+++ Assets/VultureXMusicPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VultureXMusicPlayer : MonoBehaviour
+{
+    public Vector2 vec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSource myAudioSource = GetComponent<AudioSource>();
+        //myAudioSource.mute = true;
+
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "VultureXMusicPlayer")
+                {
+                    Debug.Log("My object is clicked by mouse");
+                   
+                    if (myAudioSource != null)
+                    {
+                        myAudioSource.mute = !myAudioSource.mute;
+                        Debug.Log("AudioSource");
+                    }
+                }
+            }
+        }
+    }
+}
